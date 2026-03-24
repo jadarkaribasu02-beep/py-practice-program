9756909f29ba97a311bcdc9432aa93b07334e736
karibasu jadar <jadarkaribasu02@gmail.com>
2026-03-24 22:48:34 +0530

    n

--- loops.py
+++ loops.py
@@ -2,8 +2,20 @@
 
 k = [1,2,3,4,5,6,7,8,9]
 
-i = 0
+i = 0 #initialization
 while i < len(k):
     print(k[i]) #k[o],k[1],k[2]....
    
     i += 1
+
+ #fonding value in tupple
+     
+k = (1,2,3,4,5,6,7,8,9)    
+x = int(input('enter number u want to find:'))
+
+i = 0
+
+while i < len(k):
+    if(k[i] == x):
+        print('found at index',i)
+    i += 1    
